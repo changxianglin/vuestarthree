3911aa46359d6b43860884fa3d23b57d67b9022b
changxianglin <281532612@qq.com>
2020-10-03 00:49:29 +0800

    vue teleport

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,12 +2,18 @@
   <h1>{{ msg }}</h1>
   <button @click="data.count++">count is: {{ data.count }}</button>
   <p>Edit <code>components/HelloWorld.vue</code> to test hot module replacement.</p>
+  <!-- ModalButon -->
+<ModalButon></ModalButon>
 </template>
 
 <script>
 import { reactive } from 'vue'
+import ModalButon from './ModalButton.vue'
 export default {
   name: 'HelloWorld',
+  components: {
+    ModalButon,
+  },
   props: {
     msg: String
   },
